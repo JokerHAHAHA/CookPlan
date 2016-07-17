7c303625749a9c527589ba108af269807176ee59
jokerhahaha <liondejude@hotmail.com>
2016-07-17 22:36:50 +0200

    component for modal aperitif

---
+++ public/js/components/modalMealAperitif.js
@@ -0,0 +1,6 @@
+(function(app){
+	app.component('modalMealAperitif',{
+      templateUrl: "views/modals/modalMealAperitif.html"
+      // controller : 'mealCtrl'
+  	})
+})(angular.module('app.components'))
