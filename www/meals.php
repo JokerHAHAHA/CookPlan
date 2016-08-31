d8879dd504df6441a567bcdf3cbe5b39e24711d0
jokerhahaha <liondejude@hotmail.com>
2016-08-31 19:36:16 +0200

    poo logout & delete session php

--- www/meals.php
+++ www/meals.php
@@ -22,16 +22,28 @@ Autoloader::require();
 $auth_user = new User();
 
 //check if connected
-if($auth_user->is_loggedin()){
+if($auth_user->is_loggedin())
+{
+    //manage logout
+    if(isset($_POST['btn-signout']))
+    {
+        $auth_user->doLogout();
+        $auth_user->redirect('../index.php');
+    }
+    else
+    {
+        //load datas
+        $user_id = $_SESSION['user_session'];
+        $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+        $stmt->execute(array(":user_id"=>$user_id));
 
-    $user_id = $_SESSION['user_session'];
-    $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
-    $stmt->execute(array(":user_id"=>$user_id));
+        $user=$stmt->fetch(PDO::FETCH_ASSOC);
 
-    $user=$stmt->fetch(PDO::FETCH_ASSOC);
+        echo $twig->render('meals.html.twig', array('user'=>$user));
+    }
 
-    echo $twig->render('meals.html.twig', array('user'=>$user));
-
-}else{
+}
+else
+{
     $auth_user->redirect('../index.php');
 }
