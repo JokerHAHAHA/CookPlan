68fd1f268640ff5220c18ca2f5baf14f061f00eb
jokerhahaha <liondejude@hotmail.com>
2016-09-04 17:25:36 +0200

    sign up + errorMsg

--- www/addMeal.php
+++ www/addMeal.php
@@ -20,50 +20,88 @@ $twig = new Twig_Environment($loader, array(
 require '../src/Autoloader.php';
 Autoloader::require();
 
-if (isset($_POST['add-meal'])) {
-$cat = $_GET['cat'];
+$auth_user = new User();
 
-    switch ($cat) {
-        case 'UN APERITIF':
+//check if connected
+if($auth_user->is_loggedin())
+{
+    //manage logout
+    if(isset($_POST['btn-signout']))
+    {
+        $auth_user->doLogout();
+        $auth_user->redirect('../index.php');
+    }
+    //manage add meal
+    else if (isset($_POST['add-meal']))
+    {
+        $cat = $_GET['cat'];
+
+        switch ($cat)
+        {
+            case 'UN APERITIF':
             $category = 'aperitif';
             break;
-        case 'UNE ENTREE':
+            
+            case 'UNE ENTREE':
             $category = 'starter';
             break;
-        case 'UN PLAT':
+            
+            case 'UN PLAT':
             $category = 'meal';
             break;
-        case 'UN ACCOMPAGNEMENT':
+            
+            case 'UN ACCOMPAGNEMENT':
             $category = 'garnish';
             break;
-        case 'UN DESSERT':
+            
+            case 'UN DESSERT':
             $category = 'dessert';
             break;
-        case 'UNE BOISSON':
+            
+            case 'UNE BOISSON':
             $category = 'drink';
             break;
-    }
-    $resp = Meal::create($_POST['name'], $category, $_POST['type'], $_SESSION['user_session']);
+        }
+        $resp = Meal::create($_POST['name'], $category, $_POST['type'], $_SESSION['user_session']);
 
-    if ($resp) {
-        echo $twig->render("addMeal.html.twig", array(
-            'title'=>$_GET['cat'],
-            'save' =>$_GET['cat']." DE PLUS"
-            ));
+        if ($resp)
+        {
+            //load datas
+            $currentUser_id = $_SESSION['user_session'];
+            $user = User::findOne($currentUser_id);
+            echo $twig->render("addMeal.html.twig", array(
+                'user' =>$user,
+                'title'=>$_GET['cat'],
+                'msg' =>$_GET['cat']." DE PLUS"
+                ));
+        }
+        else
+        {
+            //load datas
+            $currentUser_id = $_SESSION['user_session'];
+            $user = User::findOne($currentUser_id);
+            echo $twig->render("addMeal.html.twig", array(
+                'title'=>$_GET['cat'],
+                'msg' =>"IL Y A EU UN PROBLEME AVEC L'ENREGISTREMENT"
+                ));
+        }
     }
     else
     {
+        //load datas
+        $currentUser_id = $_SESSION['user_session'];
+        $user = User::findOne($currentUser_id);
         echo $twig->render("addMeal.html.twig", array(
-            'title'=>$_GET['cat'],
-            'save' =>"IL Y A EU UN PROBLEME AVEC L'ENREGISTREMENT"
+            'user' =>$user,
+            'title'=>$_GET['cat']
             ));
     }
 }
 else
 {
-    echo $twig->render("addMeal.html.twig", array('title'=>$_GET['cat']));
+    $auth_user->redirect('../index.php');
 }
-    
+
 
 
 
--- www/login.php
+++ www/login.php
@@ -24,23 +24,81 @@ if($login->is_loggedin()!="")
     $login->redirect('./meals.php');
 }
 
+// SIGN IN
 if(isset($_POST['btn-signin']))
 {
-    $uname = strip_tags($_POST['login']);
-    $umail = strip_tags($_POST['login']);
-    $upass = strip_tags($_POST['password']);
+    $name = strip_tags($_POST['login']);
+    $email = strip_tags($_POST['login']);
+    $password = strip_tags($_POST['password']);
 
-    if($login->doLogin($uname,$umail,$upass))
+    if($login->doLogin($name,$email,$password))
     {
         header("Location: meals.php");
     }
     else
     {
-        echo $twig->render('login.html.twig', array('error' => "Les informations sont incorrectes"));
+        echo $twig->render('login.html.twig', array('errorSignin' => "Les informations sont incorrectes"));
+    }   
+}
+// SIGN UP
+else if(isset($_POST['btn-signup']))
+{
+    $login = strip_tags($_POST['login']);
+    $email = strip_tags($_POST['email']);
+    $password = strip_tags($_POST['password']);   
+    
+    if($login=="")
+    {
+        $error['login'] = "merci de renseigner votre pseudo";
+        echo $twig->render('login.html.twig', array('errorSignup'=>$error));    
+    }
+    else if($email=="")
+    {
+        $error['email'] = "merci de renseigner votre address mail";
+        echo $twig->render('login.html.twig', array('errorSignup'=>$error));     
+    }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        $error['email'] = 'entrez une adresse mail valide';
+        echo $twig->render('login.html.twig', array('errorSignup'=>$error)); 
+    }
+    else if($password=="")
+    {
+        $error['password'] = "merci de renseigner un mot de passe";
+        echo $twig->render('login.html.twig', array('errorSignup'=>$error)); 
+    }
+    else if(strlen($password) < 6)
+    {
+        $error['password'] = "votre mot de passe doit avoir au moins 6 caratarères,
+                                sinon on va vous piquer vos recettes : Pirates !";
+        echo $twig->render('login.html.twig', array('errorSignup'=>$error));  
+    }
+    else
+    {
+        $resp = User::register($login,$email,$password);
+        if(is_array($resp))
+        {
+            echo $twig->render('login.html.twig', array('errorSignup'=>$resp));
+        }
+        else
+        {
+            header("Location: meals.php");
+        }
     }   
 }
 else
 {
     echo $twig->render('login.html.twig');
 }
+
+
+
+
+
+
+
+
+
+
+
 
